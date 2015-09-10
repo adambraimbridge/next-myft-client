778d7a71a0568f9e60179128b2c816559c9c8a9d
phamann <patrickhamann@gmail.com>
2015-09-10 11:10:18 +0100

    http

--- myft-npm.js
+++ myft-npm.js
@@ -3,7 +3,7 @@
 var MyFTApi = require('./src/myft-api.js');
 
 module.exports = new MyFTApi({
-	apiRoot: 'http://next.ft.com/__myft/api/onsite/',
+	apiRoot: 'https://next.ft.com/__myft/api/onsite/',
 	headers: {
 		'X-API-KEY': process.env.USER_PREFS_API_KEY
 	}
