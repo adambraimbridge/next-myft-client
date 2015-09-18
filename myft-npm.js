8186377d682ef573adfb878a17bc1fb3bde2dd1f
Patrick Hamann <patrick.hamann@ft.com>
2015-09-18 12:06:19 +0100

    Allow use of MYFT_API env var

--- myft-npm.js
+++ myft-npm.js
@@ -5,6 +5,6 @@ var MyFTApi = require('./src/myft-api.js');
 module.exports = new MyFTApi({
 	apiRoot: 'https://ft-next-myft-api.herokuapp.com/v1/',
 	headers: {
-		'X-API-KEY': process.env.USER_PREFS_API_KEY
+		'X-API-KEY': process.env.USER_PREFS_API_KEY || process.env.MYFT_API_KEY
 	}
 });
