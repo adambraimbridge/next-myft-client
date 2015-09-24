f9cae490c73fb7198d66c48cef84160f06b629c5
Arjun Gadhia <arjungadhia@gmail.com>
2015-09-24 14:14:26 +0100

    Revert "Update write root"
    This reverts commit d585bf4a4b641c50d209a4293d5fa36563c62deb.

--- myft-bower.js
+++ myft-bower.js
@@ -4,5 +4,5 @@ import MyFtClient from './src/myft-client.js';
 
 export default new MyFtClient({
 	apiReadRoot: 'https://myft-api.ft.com/v1/',
-	apiWriteRoot: 'https://ft-next-myft-api.herokuapp.com/v1/'
+	apiWriteRoot: 'https://ft-next-myft-api.herokuapp.com/v1/',
 });
