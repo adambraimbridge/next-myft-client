6f26d0a49a708089d58ba8f8017464d2bc48be0c
Arjun Gadhia <arjungadhia@gmail.com>
2015-10-02 11:36:10 +0100

    Allow pagination parameters

--- tests/myft-api.spec.js
+++ tests/myft-api.spec.js
@@ -1,16 +1,34 @@
 /*global describe, it, expect, beforeEach, afterEach*/
 /*jshint expr:true*/
 'use strict';
-const MyFtApi = require('../src/myft-api');
+require('isomorphic-fetch');
 
+const MyFtApi = require('../src/myft-api');
 const myFtApi = new MyFtApi({
-	apiRoot: 'https://test-api-route.com',
+	apiRoot: 'https://test-api-route.com/',
 	headers: {
 		'X-API-KEY': 'adasd'
 	}
 });
 
+const sinon = require('sinon');
+
+
+function mockFetch(response, status) {
+	return new Promise(function(resolve) {
+		resolve({
+			ok: true,
+			status: status || 200,
+			json: function() {
+				return Promise.resolve(response);
+			}
+		});
+	});
+}
+
+
 describe('url personalising', function () {
+
 	it('should be possible to personalise a url', function () {
 
 		const testUuid = 'abcd';
@@ -34,3 +52,36 @@ describe('identifying immutable URLs', function () {
 		expect(myFtApi.isImmutableUrl('/myft/my-news/')).to.be.false;
 	})
 });
+
+describe('getting a relationship', function() {
+	let fetchStub;
+	beforeEach(function() {
+		fetchStub = sinon.stub(window, 'fetch');
+		fetchStub.returns(mockFetch([]));
+	});
+
+	afterEach(function() {
+		window.fetch.restore();
+	});
+
+
+	it('should request the API', function(done) {
+		myFtApi.getAllRelationship('user', 'userId', 'followed').then(function() {
+			expect(fetchStub.calledWith('https://test-api-route.com/user/userId/followed')).to.be.true;
+			done();
+		})
+		.catch(done);
+
+	});
+
+	it('should accept pagination parameters', function(done) {
+		myFtApi.getAllRelationship('user', 'userId', 'followed', {
+			page: 2,
+			limit: 10
+		}).then(function() {
+			expect(fetchStub.calledWith('https://test-api-route.com/user/userId/followed?page=2&limit=10')).to.be.true;
+			done();
+		}).catch(done);
+
+	})
+})
