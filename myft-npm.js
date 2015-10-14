ee2941906da1bfacdc39e0e7f1ef782cd0a3e7bd
Laurie Boyes <laurie.boyes@ft.com>
2015-10-14 14:17:32 +0100

    can overwrite api root with env variable

--- myft-npm.js
+++ myft-npm.js
@@ -3,7 +3,7 @@
 var MyFTApi = require('./src/myft-api.js');
 
 module.exports = new MyFTApi({
-	apiRoot: 'https://myft-api.ft.com/v1/',
+	apiRoot: process.env.TEST_MYFT_API_ROOT || 'https://myft-api.ft.com/v1/',
 	headers: {
 		'X-API-KEY': process.env.USER_PREFS_API_KEY || process.env.MYFT_API_KEY
 	}
