822f5e8a83b76c0eb5436b687155a2f237b1bbcb
Laurie Boyes <laurie.boyes@ft.com>
2015-10-27 16:25:24 +0000

    remove dependency on MYFT_API_URL environment variable, which became a breaking change for version 3.x.x

--- myft-npm.js
+++ myft-npm.js
@@ -3,7 +3,7 @@
 var MyFTApi = require('./src/myft-api.js');
 
 module.exports = new MyFTApi({
-	apiRoot: process.env.MYFT_API_URL,
+	apiRoot: process.env.MYFT_API_URL || 'https://myft-api.ft.com/v1/',
 	headers: {
 		'X-API-KEY': process.env.USER_PREFS_API_KEY || process.env.MYFT_API_KEY
 	}
