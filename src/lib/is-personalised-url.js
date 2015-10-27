9b7c9aeecf114eab14f4fe50966c13ee8a40060c
Laurie Boyes <laurie.boyes@ft.com>
2015-10-27 11:05:29 +0000

    made url personalisation allow for uuids in the query string

--- src/lib/is-personalised-url.js
+++ src/lib/is-personalised-url.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function (url) {
-	return /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/.test(url);
+	return /\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/.test(url);
 };
