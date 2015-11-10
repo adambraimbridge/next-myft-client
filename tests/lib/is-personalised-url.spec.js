7d5f6c0625f37a44f613a53dd7d085c0e98e8d70
Laurie Boyes <laurie.boyes@ft.com>
2015-11-10 17:37:48 +0000

    Whoops! Didn't realise myft isn't always part of the path when we're checking for personalisation

--- tests/lib/is-personalised-url.spec.js
+++ tests/lib/is-personalised-url.spec.js
@@ -8,14 +8,14 @@ var isPersonalisedUrl = require('../../src/lib/is-personalised-url');
 describe('identifying personalised URLs', function () {
 	it('should identify between personalised urls and not personalised urls', function () {
 
-		expect(isPersonalisedUrl('/myft/3f041222-22b9-4098-b4a6-7967e48fe4f7')).to.be.true;
-		expect(isPersonalisedUrl('/myft/my-news/3f041222-22b9-4098-b4a6-7967e48fe4f7')).to.be.true;
+		expect(isPersonalisedUrl('/3f041222-22b9-4098-b4a6-7967e48fe4f7')).to.be.true;
+		expect(isPersonalisedUrl('/my-news/3f041222-22b9-4098-b4a6-7967e48fe4f7')).to.be.true;
 
-		expect(isPersonalisedUrl('/myft/product-tour')).to.be.false;
-		expect(isPersonalisedUrl('/myft/my-news/')).to.be.false;
-		expect(isPersonalisedUrl('/myft/portfolio/')).to.be.false;
+		expect(isPersonalisedUrl('/product-tour')).to.be.false;
+		expect(isPersonalisedUrl('/my-news/')).to.be.false;
+		expect(isPersonalisedUrl('/portfolio/')).to.be.false;
 
-		expect(isPersonalisedUrl('/myft/list/e077a74b-693f-4744-b055-d239f548f356/')).to.be.false;
+		expect(isPersonalisedUrl('/list/e077a74b-693f-4744-b055-d239f548f356/')).to.be.false;
 		// ...
 
 	})
