3def90861c9ad2788fedcf95a2d7b74388a613e5
phamann <patrickhamann@gmail.com>
2015-12-02 12:40:06 +0000

    Remove trailing slash from list imuutable regex

--- src/lib/is-immutable-url.js
+++ src/lib/is-immutable-url.js
@@ -5,6 +5,6 @@ var isPersonalisedUrl = require('./is-personalised-url');
 module.exports = function (url) {
 	return /^\/(__)?myft\/api\//.test(url) ||
 		/^\/(__)?myft\/product-tour/.test(url) ||
-		/^\/(__)?myft\/list\//.test(url) ||
+		/^\/(__)?myft\/list/.test(url) ||
 		isPersonalisedUrl(url);
 };
