6d2ebbea56b1935a68be4b04461b297fe37cb1b0
phamann <patrickhamann@gmail.com>
2015-12-23 14:12:08 +0000

    Remove old user-prefs key from server side requests

--- myft-npm.js
+++ myft-npm.js
@@ -5,6 +5,6 @@ var MyFTApi = require('./src/myft-api.js');
 module.exports = new MyFTApi({
 	apiRoot: process.env.MYFT_API_URL,
 	headers: {
-		'X-API-KEY': process.env.USER_PREFS_API_KEY || process.env.MYFT_API_KEY
+		'X-API-KEY': process.env.MYFT_API_KEY
 	}
 });
