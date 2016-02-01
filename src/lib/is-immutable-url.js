2b76a1186bdf0733d62d55c253448f600745c2e3
Keith Rogers <git@keithrogers.eu>
2016-02-01 19:43:08 +0000

    Add isLegacyUrl module

--- src/lib/is-immutable-url.js
+++ src/lib/is-immutable-url.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const isPersonalisedUrl = require('./is-personalised-url');
+const isLegacyUrl = require('./is-legacy-url');
 
 module.exports = url => /^\/(__)?myft\/api\//.test(url) ||
 	/^\/(__)?myft\/product-tour/.test(url) ||
 	/^\/(__)?myft\/list/.test(url) ||
+	isLegacyUrl(url) ||
 	isPersonalisedUrl(url);
