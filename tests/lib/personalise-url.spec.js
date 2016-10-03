e93dd640b1442a080156223ee483d9189c5dd7a0
Keith Rogers <git@keithrogers.eu>
2016-10-03 10:25:16 +0100

    Add another clippings test

--- tests/lib/personalise-url.spec.js
+++ tests/lib/personalise-url.spec.js
@@ -37,6 +37,7 @@ describe('url personalising', function () {
 		expect(personaliseUrl('/myft/my-topics', userId)).to.equal('/myft/my-topics');
 		expect(personaliseUrl('/myft/preferences', userId)).to.equal('/myft/preferences');
 		expect(personaliseUrl('/myft/product-tour', userId)).to.equal('/myft/product-tour');
+		expect(personaliseUrl('/myft/clippings', userId)).to.equal('/myft/clippings');
 
 		// a url with a non-user uuid in the query string
 		expect(personaliseUrl(`/myft/saved-articles?fragment=true&contentId=${articleId}`, userId)).to.equal(`/myft/saved-articles/${userId}?fragment=true&contentId=${articleId}`);
